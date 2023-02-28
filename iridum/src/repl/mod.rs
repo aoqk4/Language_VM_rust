d4d586808ddd1c7b545ee138d951cd8eab5cb4a6
qazplm807733@gmail.com <qazplm807733@gmail.com>
2023-02-28 16:45:58 +0900

    fix REPL

--- iridum/src/repl/mod.rs
+++ iridum/src/repl/mod.rs
@@ -1,9 +1,13 @@
+use nom::types::CompleteStr;
+
 use crate::assembler::program_parser::program;
 use crate::vm::VM;
 use std;
-use std::io;
-use std::io::Write;
+use std::fs::File;
+use std::io::{self, stdin};
+use std::io::{Read, Write};
 use std::num::ParseIntError;
+use std::path::Path;
 
 pub struct REPL {
     // 위키 눌렀을 때? 명령 그래도 유지하기 위해서
@@ -89,6 +93,32 @@ impl REPL {
                     println!("Listing registers and all contents : ");
                     println!("{:#?}", self.vm.registers);
                     println!("End of Register Listing");
+                }
+                ".load_file" => {
+                    print!("Please enter the path to the file you wish to load: ");
+                    io::stdout().flush().expect("Unable to flush stdout");
+                    let mut tmp = String::new();
+
+                    stdin()
+                        .read_line(&mut tmp)
+                        .expect("Unable to read line from user");
+                    let tmp = tmp.trim();
+                    let filename = Path::new(&tmp);
+
+                    let mut f = File::open(Path::new(&filename)).expect("file not found");
+                    let mut contents = String::new();
+
+                    f.read_to_string(&mut contents)
+                        .expect("There was an error reading from the file");
+
+                    let program = match program(CompleteStr(&contents)) {
+                        Ok((remainder, program)) => program,
+                        Err(e) => {
+                            println!("Unable to parse input: {:?}", e);
+                            continue;
+                        }
+                    };
+                    self.vm.program.append(&mut program.to_bytes());
                 }
                 _ => {
                     let program = match program(buffer.into()) {
