f5659f17a4f0afa96e1d3767f98d427c4397ab6c
qazplm807733@gmail.com <qazplm807733@gmail.com>
2023-03-05 20:07:51 +0900

    add thread base

--- iridium-0.0.16/src/main.rs
+++ iridium-0.0.16/src/main.rs
@@ -1,6 +1,6 @@
-use std::path::Path;
 use std::fs::File;
 use std::io::prelude::*;
+use std::path::Path;
 
 #[macro_use]
 extern crate nom;
@@ -8,14 +8,15 @@ extern crate nom;
 extern crate clap;
 #[macro_use]
 extern crate log;
-extern crate env_logger;
 extern crate byteorder;
+extern crate env_logger;
 
 use clap::App;
 
 pub mod assembler;
 pub mod instruction;
 pub mod repl;
+pub mod scheduler;
 pub mod vm;
 
 fn main() {
@@ -35,12 +36,10 @@ fn main() {
                     vm.add_bytes(p);
                     vm.run();
                     std::process::exit(0);
-                },
-                Err(_e) => {
-
                 }
+                Err(_e) => {}
             }
-        },
+        }
         None => {
             start_repl();
         }
@@ -55,21 +54,19 @@ fn start_repl() {
 fn read_file(tmp: &str) -> String {
     let filename = Path::new(tmp);
     match File::open(Path::new(&filename)) {
-      Ok(mut fh) => {
-        let mut contents = String::new();
-        match fh.read_to_string(&mut contents) {
-          Ok(_) => {
-            contents
-          },
-          Err(e) => {
-            println!("There was an error reading file: {:?}", e);
-            std::process::exit(1);
-          }
+        Ok(mut fh) => {
+            let mut contents = String::new();
+            match fh.read_to_string(&mut contents) {
+                Ok(_) => contents,
+                Err(e) => {
+                    println!("There was an error reading file: {:?}", e);
+                    std::process::exit(1);
+                }
+            }
         }
-      },
-      Err(e) => {
-        println!("File not found: {:?}", e);
-        std::process::exit(1)
-      }
+        Err(e) => {
+            println!("File not found: {:?}", e);
+            std::process::exit(1)
+        }
     }
 }
